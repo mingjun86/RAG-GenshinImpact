349040fc8c1ed7cd70099f8f608539f6543429ff
Mingjun <your_email>
2026-05-19 20:01:19 +0800

    小调整06

--- config_data.py
+++ config_data.py
@@ -6,10 +6,10 @@ collection_name = "rag"
 persist_directory = "./faiss_db"
 
 # 文本分割配置
-chunk_size = 1200
-chunk_overlap = 150
+chunk_size = 800
+chunk_overlap = 100
 separators = ["\n\n", "\n", ".", "?", "!", "。", "？", "！", " ", ""]
-max_split_char_number = 2000  # 文本分割的阈值
+max_split_char_number = 1000  # 文本分割的阈值
 
 # 检索配置
 similarity_threshold = 5  # 检索返回匹配的文档数量
